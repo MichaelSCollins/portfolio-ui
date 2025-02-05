b3c66b7dde781969ddbe794f74db9d7f1910f62a
Mike Collins <michaelscollins.it@gmail.com>
2025-02-04 19:23:29 -0800

    asdasdasd

--- app/company/[id]/page.tsx
+++ app/company/[id]/page.tsx
@@ -1,23 +1,19 @@
-import Image from 'next/image';
 import Link from 'next/link';
-import { RiArrowDropLeftFill } from "react-icons/ri"; 
-import img from '@/public/img/page-content/blue-guy-4.webp'
-import { BasicInfo, Screenshots, Achievements, Skills } from '@/app/company/[id]/sections';
+import { RiArrowDropLeftFill } from "react-icons/ri";
+import { BasicInfo, Screenshots, Achievements } from '@/app/company/[id]/sections';
 import ExperienceItem from '@/interfaces/ExperienceItem'
 import experiences from '@/.fake/experience'
-import fadeInLoadingAnimation from '@/styles/animations/fadeInAnimation';
 import FadeInImage from '@/components/Image/FadeInImage';
 import StackedBarChart from './sections/chart';
-let loaded = true;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function CompanyPage({ params: { id } }: any) {
     const experience: ExperienceItem = experiences[+id]
     return (
         <div className='sm:px-12 max-sm:px-6  w-full'>
             <div className={'flex flex-col '}>
-                <Link className='mt-8 text-secondary max-sm:mx-auto flex text-sm *:hover:text-primary' 
+                <Link className='mt-8 text-secondary max-sm:mx-auto flex text-sm *:hover:text-primary'
                     href="/experience">
-                    <RiArrowDropLeftFill className="text-foreground text-3xl" /> 
+                    <RiArrowDropLeftFill className="text-foreground text-3xl" />
                     <p className="pt-1">Back to Experience</p>
                 </Link>
                 <div className={"flex flex-col w-full gap-4"}>
@@ -30,19 +26,19 @@ function CompanyPage({ params: { id } }: any) {
 
                     <div className='flex max-sm:flex-col sm:mt-10'>
                         <div className='w-full h-full grow flex justify-between'>
-                        <FadeInImage 
-                            image={{
-                                src: '/img/page-content/blue-guy-4.webp',
-                                alt: experience.img?.alt || 'company-logo',
-                                height: 500,
-                                width: 750
-                            }}  />
+                            <FadeInImage
+                                image={{
+                                    src: '/img/page-content/blue-guy-4.webp',
+                                    alt: experience.img?.alt || 'company-logo',
+                                    height: 500,
+                                    width: 750
+                                }} />
                         </div>
                         <Achievements achievements={experience.achievements || []} />
                     </div>
                     {experience.screenshots
-                                && experience.screenshots.length
-                                && <Screenshots images={experience.screenshots || []} />}
+                        && experience.screenshots.length
+                        && <Screenshots images={experience.screenshots || []} />}
                     {/* <PieChart /> */}
                 </div>
             </div>
