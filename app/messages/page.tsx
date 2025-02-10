2a74694a424dc96b767619b31c70808a0a46396e
Mike Collins <michaelscollins.it@gmail.com>
2025-02-10 11:37:42 +0000

    inital

---
+++ app/messages/page.tsx
@@ -0,0 +1,20 @@
+const MessagePage = () => {
+    return <div className="w-full h-min-screen">
+        <table className="border border-foreground bg-secondary/50 flex flex-cols-3">
+            <thead>
+                <th>
+                    Email
+                </th>
+                <th>
+                    Subject
+                </th>
+                <th>
+                    Date Sent
+                </th>
+            </thead>
+            <tbody>
+                {}
+            </tbody>
+        </table>
+    </div>
+}
